899dae173b1ce03497e298f5aac9c7cdcb090268
Flavius Dinu <cristian@oc5487548415.ibm.com>
2017-10-17 13:39:10 +0300

    added github authentication

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,8 @@ const passport = require('passport');
 // http://passportjs.org/ get strategies
 const GoogleStrategy = require('passport-google-oauth20');
 const DiscordStrategy = require('passport-discord');
+const FacebookStrategy = require('passport-facebook');
+const GithubStrategy = require('passport-github2');
 
 const keys = require('./keys');
 
@@ -19,11 +21,35 @@ passport.use(
 
 passport.use(
   new DiscordStrategy({
-  // options for the Google Strategy
-  // console.developers.google.com
+  // options for the Discord Strategy
+  // discordapp.com/developers
     callbackURL:'/auth/discord/redirect',
     clientID: keys.discord.clientID,
     clientSecret: keys.discord.clientSecret
+  }, () => {
+  // passport callback function
+  })
+)
+
+passport.use(
+  new FacebookStrategy({
+  // options for the Facebook Strategy
+  // developers.facebook.com
+    callbackURL:'/auth/facebook/redirect',
+    clientID: keys.facebook.clientID,
+    clientSecret: keys.facebook.clientSecret
+  }, () => {
+  // passport callback function
+  })
+)
+
+passport.use(
+  new GithubStrategy({
+  // options for the Github Strategy
+  // developers.facebook.com
+    callbackURL:'/auth/github/redirect',
+    clientID: keys.github.clientID,
+    clientSecret: keys.github.clientSecret
   }, () => {
   // passport callback function
   })
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -23,13 +23,34 @@ router.get('/discord', passport.authenticate('discord', {
   scope: ['profile']
 }));
 
+// auth with facebook
+router.get('/facebook', passport.authenticate('facebook', {
+  scope: ['public_profile']
+}));
+
+// auth with github
+router.get('/github', passport.authenticate('github', {
+  scope: ['profile']
+}));
+
 // callback route for google to redirect to
 router.get('/google/redirect', (req, res) => {
   res.send('you reached the callback uri for google')
 });
 
+// discord callback
 router.get('/discord/redirect', (req, res) => {
   res.send('you reached the callback uri for discord')
+});
+
+// facebook callback
+router.get('/facebook/redirect', (req, res) => {
+  res.send('you reached the callback uri for facebook')
+});
+
+// github callback
+router.get('/github/redirect', (req, res) => {
+  res.send('you reached the callback uri for github')
 });
 
 module.exports = router;
