28fe26d836ca985fba4a50f3a6049a92b67d0f69
Flavius Dinu <cristian@oc5487548415.ibm.com>
2017-10-17 12:50:18 +0300

    discord authentication

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 // http://passportjs.org/ get strategies
 const GoogleStrategy = require('passport-google-oauth20');
+const DiscordStrategy = require('passport-discord');
 
 const keys = require('./keys');
 
@@ -11,6 +12,18 @@ passport.use(
     callbackURL:'/auth/google/redirect',
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
+  }, () => {
+  // passport callback function
+  })
+)
+
+passport.use(
+  new DiscordStrategy({
+  // options for the Google Strategy
+  // console.developers.google.com
+    callbackURL:'/auth/discord/redirect',
+    clientID: keys.discord.clientID,
+    clientSecret: keys.discord.clientSecret
   }, () => {
   // passport callback function
   })
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -18,9 +18,18 @@ router.get('/google', passport.authenticate('google', {
   scope: ['profile']
 }));
 
+// auth with discord
+router.get('/discord', passport.authenticate('discord', {
+  scope: ['profile']
+}));
+
 // callback route for google to redirect to
 router.get('/google/redirect', (req, res) => {
-  res.send('you reached the callback uri')
+  res.send('you reached the callback uri for google')
+});
+
+router.get('/discord/redirect', (req, res) => {
+  res.send('you reached the callback uri for discord')
 });
 
 module.exports = router;
