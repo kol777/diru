31c7204a974a78519dd14df58b691c5f60b282a9
Flavius Dinu <cristian@oc5487548415.ibm.com>
2017-10-19 15:12:33 +0300

    connected to mongodb

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const passportSetup = require('./config/passport-setup');
 // import routes
 const authRoutes = require('./routes/auth-routes');
 
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 const app = express();
 
 // set up ejs view
@@ -10,6 +13,12 @@ app.set('view engine', 'ejs');
 
 // set up routes
 app.use('/auth', authRoutes);
+
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () =>{
+  console.log('connected to mongodb')
+});
 
 // homepage route
 app.get('/', (req, res) => {
