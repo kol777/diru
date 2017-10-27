74aec3bafb464f345e8e1a36aec063ea865880b9
Flavius Dinu <cristian@oc5487548415.ibm.com>
2017-10-27 16:39:18 +0300

    minor changes to app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const passportSetup = require('./config/passport-setup');
 // import routes
 const authRoutes = require('./routes/auth-routes');
-const passport = require('passport');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const app = express();
 
+const passport = require('passport');
 // set up ejs view
 app.set('view engine', 'ejs');
 
@@ -15,8 +15,9 @@ app.set('view engine', 'ejs');
 app.use('/auth', authRoutes);
 
 app.use(cookieSession({
-  maxAge: 24 * 60 * 60 * 1000, // 24 hours in milisecs
-  keys: [keys.session.cookiekey]
+  // 24 hours in milisecs
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
 }));
 
 //init passportSetup
